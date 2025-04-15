92ab41070fd43a642b3c291ca123dec9ed5480ab
marcellmoura <marcelltmoura@gmail.com>
2025-04-15 10:19:18 -0300

    Adicionar formulário de transações, tela de perfil com alteração de senha e ajustes de layout

--- src/script.js
+++ src/script.js
@@ -21,3 +21,70 @@ function fazerLogin() {
     mensagemErro.textContent = "E-mail ou senha inválidos.";
   }
 }
+
+const botaoNovaTransacao = document.getElementById("abrirFormulario");
+const formulario = document.getElementById("formularioTransacao");
+
+botaoNovaTransacao.addEventListener("click", () => {
+  if (formulario.style.display === "none") {
+    formulario.style.display = "flex";
+  } else {
+    formulario.style.display = "none";
+  }
+});
+
+const botaoCancelar = document.getElementById("cancelarFormulario");
+
+botaoCancelar.addEventListener("click", () => {
+  formulario.style.display = "none";
+});
+
+const botaoSalvar = document.getElementById("salvarTransacao");
+const tabela = document.querySelector(".tabela-transacoes");
+
+botaoSalvar.addEventListener("click", () => {
+  const data = document.getElementById("data").value;
+  const descricao = document.getElementById("descricao").value.trim();
+  const categoria = document.getElementById("categoria").value;
+  const valor = document.getElementById("valor").value;
+  const tipo = document.querySelector(".botao-tipo.ativo").dataset.tipo;
+
+  if (!data || !descricao || !valor) {
+    alert("Preencha todos os campos.");
+    return;
+  }
+
+  const novaLinha = document.createElement("div");
+  novaLinha.classList.add("tabela-linha");
+
+  novaLinha.innerHTML = `
+    <span>${data}</span>
+    <span>${descricao}</span>
+    <span>${categoria}</span>
+    <span class="${tipo}">${tipo.charAt(0).toUpperCase() + tipo.slice(1)}</span>
+    <span>R$ ${parseFloat(valor).toFixed(2)}</span>
+  `;
+
+  tabela.appendChild(novaLinha);
+
+  formulario.style.display = "none";
+
+  // limpa o formulário
+  document.getElementById("data").value = "";
+  document.getElementById("descricao").value = "";
+  document.getElementById("categoria").selectedIndex = 0;
+  document.getElementById("valor").value = "";
+});
+
+const botoesTipo = document.querySelectorAll(".botao-tipo");
+
+botoesTipo.forEach(botao => {
+  botao.addEventListener("click", () => {
+    // Remove classe 'ativo' de todos
+    botoesTipo.forEach(b => b.classList.remove("ativo"));
+    // Adiciona ao botão clicado
+    botao.classList.add("ativo");
+  });
+});
+
+
